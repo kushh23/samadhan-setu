67846ba8f8f7a0b65d30b09a64a5e074c3c7acd1
Kush Namdev <kush@themeisle.com>
2025-09-26 02:28:50 +0530

    changes

--- project/next.config.ts
+++ project/next.config.ts
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['cfafdwbhkrjzzmpqmmmo.supabase.co'], // allow this domain
   },
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
    api: {
     bodyParser: {
       sizeLimit: '100mb',
